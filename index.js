afb098d3ccddc045cca57d4f27f51ef52bc30dd9
Paulo Sergio <ngcpaulo1@gmail.com>
2023-04-03 21:21:31 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,5 @@
+// 4 
+let pergunta = Number (prompt('Quanto você ganha por hora?'));
+let pergunta2 = Number (prompt('Quantas horas você trabalha por mês?'));
+console.log('Ser salário é', pergunta * pergunta2); 
+
